6e6a866cac882181998833f208f37d9e2b2f3a41
Saloni Oswal <salonioswal22@gmail.com>
2024-03-23 21:05:23 -0400

    Updated traffic light config

--- src/trafficLightConfig.tsx
+++ src/trafficLightConfig.tsx
@@ -1,28 +1,28 @@
 type Lights = {
-  backgroundColor: string,
-  duration: number,
-  nextColor: string,
+  backgroundColor: string;
+  duration: number;
+  nextColor: string;
 };
 
 type trafficLightsTypes = {
-  [key: string]: Lights,
+  [key: string]: Lights;
 };
 
 const trafficLights: trafficLightsTypes = {
   red: {
     backgroundColor: "red",
     duration: 4000,
-    nextColor: "yellow",
+    nextColor: "green",
   },
   yellow: {
     backgroundColor: "yellow",
     duration: 500,
-    nextColor: "green",
+    nextColor: "red",
   },
   green: {
     backgroundColor: "green",
     duration: 3000,
-    nextColor: "red",
+    nextColor: "yellow",
   },
 };
 
